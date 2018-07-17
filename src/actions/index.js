21c023432a2ac7c959cba367ef1c67fe891a49bc
Digant Patel <digantpatel92@gmail.com>
2018-07-17 11:28:08 +0530

    Added reducers and action initialization files

---
+++ src/actions/index.js
@@ -0,0 +1,1 @@
+// Export all action functions from here
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+// Import all reducers here and combine them in rootReducer
+
+const rootReducer = combineReducers({
+    // weather: WeatherReducer
+});
+  
+export default rootReducer;
